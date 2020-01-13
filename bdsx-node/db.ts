4170496f248e3aa74506edcda65259bbe8f86c30
karikera <karikera3@hotmail.com>
2020-01-13 16:21:36 +0900

    bdsx@1.1.20 change db rule

--- bdsx-node/db.ts
+++ bdsx-node/db.ts
@@ -1,6 +1,5 @@
 
 import native = require('./native');
-import Super = native.MariaDB;
 
 class SqlError extends Error
 {
@@ -29,84 +28,18 @@ if (!Promise.prototype.finally)
 		return this.then(voiding, voiding);
     };
 }
-    
-export class MariaDB extends native.MariaDB implements Promise<void>
+
+export class MariaDBTransaction
 {
-    private worker:Promise<any>;
-
-    constructor(host?:string, username?:string, password?:string, db?:string, port?:number)
+    constructor(private readonly db:native.MariaDB)
     {
-        super((error, errno)=>{
-            if (error)
-            {
-                reject(new SqlError(error, errno!));
-            }
-            else
-            {
-                resolve();
-            }
-        }, host, username, password, db, port);
-        var resolve:()=>void;
-        var reject:(err:Error)=>void;
-        this.worker = new Promise((_resolve, _reject)=>{
-            resolve = _resolve;
-            reject = _reject;
-        });
     }
 
-    get [Symbol.toStringTag]():string
-    {
-        return 'MariaDB';
-    }
-
-    then<TResult1 = void, TResult2 = never>(onfulfilled?: ((value:void) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2>
-    {
-        return this.worker.then(onfulfilled, onrejected);
-    }
-
-    catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null): Promise<void | TResult>
-    {
-        return this.worker.catch(onrejected);
-    }
-    
-    finally(onfinally?: (() => void) | undefined | null): Promise<void>
-    {
-        return this.worker.finally(onfinally);
-    }
-
-    transaction<T>(func:()=>Promise<T>):Promise<T>
-    {
-        const ret = this.worker.then(()=>{
-            this.ready();
-        }).then(func);
-        this.worker = ret.then(()=>{
-            this.commit();
-        }, ()=>{
-            this.rollback();
-        });
-        return ret;
-    }
-
-    execute(query:string):Promise<string[][]>
-    {
-        return this.transaction(async()=>{
-            Super.prototype.query.call(this, query);
-            const out:string[][] = [];
-
-            let res:string[]|null;
-            while (res = await this.fetch())
-            {
-                out.push(res);
-            }
-            this.closeResult();
-            return out;
-        });
-    }
 
     query(query:string):Promise<number>
     {
         return new Promise((resolve, reject)=>{
-            Super.prototype.query.call(this, query, (error:string, fieldCount:number)=>{
+            this.db.query(query, (error:string, fieldCount:number)=>{
                 if (error) reject(Error(error));
                 else resolve(fieldCount);
             });
@@ -116,9 +49,79 @@ export class MariaDB extends native.MariaDB implements Promise<void>
     fetch():Promise<string[]|null>
     {
         return new Promise(resolve=>{
-            Super.prototype.fetch.call(this, (row:string[]|null)=>{
+            this.db.fetch((row:string[]|null)=>{
                 resolve(row);
             });
+        });
+    }
+
+}
+    
+export class MariaDB
+{
+    private worker:Promise<any>;
+    private readonly db:native.MariaDB;
+    private readonly tran:MariaDBTransaction;
+
+    constructor(host?:string, username?:string, password?:string, db?:string, port?:number)
+    {
+        var resolve:()=>void;
+        var reject:(err:Error)=>void;
+        this.worker = new Promise((_resolve, _reject)=>{
+            resolve = _resolve;
+            reject = _reject;
+        });
+        this.db = new native.MariaDB((error, errno)=>{
+            if (error)
+            {
+                reject(new SqlError(error, errno!));
+            }
+            else
+            {
+                resolve();
+            }
+        }, host, username, password, db, port);
+        this.tran = new MariaDBTransaction(this.db);
+    }
+
+    get [Symbol.toStringTag]():string
+    {
+        return 'MariaDB';
+    }
+
+    transaction<T>(func:(tran:MariaDBTransaction)=>Promise<T>):Promise<T>
+    {
+        const ret = this.worker.then(()=>{
+            this.db.ready();
+        }).then(()=>func(this.tran));
+        this.worker = ret.then(()=>{
+            this.db.commit();
+        }, ()=>{
+            this.db.rollback();
+        });
+        return ret;
+    }
+
+    query(query:string):void
+    {
+        this.worker.then(()=>{
+            this.db.query(query);
+        });
+    }
+
+    execute(query:string):Promise<string[][]>
+    {
+        return this.transaction(async()=>{
+            this.db.query(query);
+            const out:string[][] = [];
+
+            let res:string[]|null;
+            while (res = await this.tran.fetch())
+            {
+                out.push(res);
+            }
+            this.db.closeResult();
+            return out;
         });
     }
 }
