e500e95664d5ca4ea0dc056ee5a732878d06b856
karikera <karikera3@hotmail.com>
2021-01-17 03:22:56 +0900

    undefined to 1.0.0.1

--- bdsx/checkcore.ts
+++ bdsx/checkcore.ts
@@ -6,7 +6,7 @@ import colors = require('colors');
 if (cgate.bdsxCoreVersion !== version.coreVersion)
 {
     console.error(colors.red('BDSX Core Version is unmatched'));
-    console.error(colors.red(`Old: ${cgate.bdsxCoreVersion}`));
+    console.error(colors.red(`Old: ${cgate.bdsxCoreVersion || '1.0.0.1'}`));
     console.error(colors.red(`New: ${version.coreVersion}`));
     console.log("Please use 'npm run install_bds' to update it");
     process.exit(0);
