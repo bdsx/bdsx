e6fd3fad9b038fe11ee6e94879eb0a73ea4be478
karikera <karikera3@hotmail.com>
2021-01-24 17:50:46 +0900

    node.d.ts - clean

--- bdsx/externs/node.d.ts
+++ bdsx/externs/node.d.ts
@@ -1,28 +1,12 @@
 
-interface IExecuteCommandCallbackFix extends IExecuteCommandCallback {
-    data: {
-        message:string;
-        statucMessage: string;
-        statusCode: number;
+declare namespace NodeJS {
+    interface Process
+    {
+        jsEngine?:'chakracore';
+    }
+    interface ProcessVersions
+    {
+        chakracore?:string;
+        // bedrock_server:string; // it does not exists yet
     }
 }
-
-declare global
-{
-    namespace NodeJS {
-        interface Process
-        {
-            jsEngine?:'chakracore';
-        }
-        interface ProcessVersions
-        {
-            chakracore?:string;
-            // bedrock_server:string; // it does not exists yet
-        }
-    }
-    interface IServerSystem<TSystem> {
-        executeCommand(command: string, callback: (callback: IExecuteCommandCallbackFix) => void): void;
-    }
-} 
-
-export {};
