4eed6f76b1323e106a7119a7ee065d5132deb7a9
karikera <karikera3@hotmail.com>
2021-01-29 04:57:06 +0900

    example.ts - writing machine codes

--- example_and_test/examples.ts
+++ example_and_test/examples.ts
@@ -267,3 +267,18 @@ console.assert(obj.c === 1 && obj.c === pointer.getInt8(6));
 // alignment test
 obj.d = 123;
 console.assert(obj.d === pointer.getInt32(8));
+
+
+// Low Level - Writing Machine Codes
+import { asm, Register } from "bdsx/assembler";
+import { proc } from "bdsx/bds/proc";
+
+const asmmain = asm()
+.sub_r_c(Register.rsp, 0x28) // make stack frame
+.mov_r_c(Register.rcx, asm.const_str('Hello World with machine codes!\n')) // rcx = "Hello World!\n";
+.call64(proc['printf'], Register.rax) // printf(rcx);
+.add_r_c(Register.rsp, 0x28) // remove stack frame
+.ret() // return
+.make(RawTypeId.Void); // make as the js function
+
+asmmain();
