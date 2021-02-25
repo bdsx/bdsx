b699aef46e1bd87fac05ac4eaf74ef3c09b7f4fc
karikera <karikera3@hotmail.com>
2021-02-26 00:41:34 +0900

    return back index.ts

--- index.ts
+++ index.ts
@@ -1,18 +1,4 @@
 
 // Please start your own codes from here!
 
-import { command } from "./bdsx";
-
-// import './example_and_test'; // remove this if it's not necessary for you
-
-
-const system = server.registerSystem(0, 0);
-
-command.hook.on((command, originName)=>{
-    return 0;
-
-});
-
-system.executeCommand('test', ()=>{});
-
-
+import './example_and_test'; // remove this if it's not necessary for you
