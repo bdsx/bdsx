da8e5e30ad820e7578b440e48200ca09606c9cb3
PneuJai <53138480+Rjlintkh@users.noreply.github.com>
2021-03-04 00:30:35 +0800

    add Actor::getNameTag

--- bdsx/bds/symbols.ts
+++ bdsx/bds/symbols.ts
@@ -21,6 +21,7 @@ const symbols = [
     'std::_Allocate<16,std::_Default_allocate_traits,0>',
     'MinecraftCommands::executeCommand',
     "ServerPlayer::`vftable'",
+    'Actor::getNameTag',
     'Actor::getUniqueID',
     'BaseAttributeMap::getMutableInstance',
     'ExtendedCertificate::getXuid',
