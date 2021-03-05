d5f30f09c855d3c996b35b88a5498d401e10b180
PneuJai <53138480+Rjlintkh@users.noreply.github.com>
2021-03-05 19:54:10 +0800

    Add Player.setName

--- bdsx/bds/player.ts
+++ bdsx/bds/player.ts
@@ -1,9 +1,21 @@
 import { abstract } from "bdsx/common";
+import { NativeType } from "bdsx/nativetype";
+import { CxxStringWrapper } from "bdsx/pointer";
 import { Actor } from "./actor";
 import { NetworkIdentifier } from "./networkidentifier";
 import { Packet } from "./packet";
 
 export class Player extends Actor {
+    protected _setName(name: CxxStringWrapper):void {
+        abstract();
+    }
+    setName(name:string):void {
+        let _name = new CxxStringWrapper(true);
+        _name[NativeType.ctor]();
+        _name.value = name;
+        this._setName(_name);
+        _name[NativeType.dtor]();
+    }
 }
 
 export class ServerPlayer extends Player {
