73271ad1f84e62903e7c52cba9904c3cc626c675
karikera <karikera3@hotmail.com>
2021-03-07 16:36:15 +0900

    Vec3.create

--- bdsx/bds/blockpos.ts
+++ bdsx/bds/blockpos.ts
@@ -16,6 +16,14 @@ export class Vec3 extends NativeClass {
     x:float32_t;
     y:float32_t;
     z:float32_t;
+
+    static create(x:number, y:number, z:number):Vec3 {
+        const v = new Vec3(true);
+        v.x = x;
+        v.y = y;
+        v.z = z;
+        return v;
+    }
 }
 Vec3.define({
     x:float32_t,
