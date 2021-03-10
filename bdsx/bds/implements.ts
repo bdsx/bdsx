89e976c4ab2ffa8c1c0db3bca03217f6f50ff02e
PneuJai <53138480+Rjlintkh@users.noreply.github.com>
2021-03-11 01:29:19 +0800

    Prevent empty item error

--- bdsx/bds/implements.ts
+++ bdsx/bds/implements.ts
@@ -388,6 +388,7 @@ ItemStack.abstract({
     amount:[uint8_t, 0x22],
 });
 (ItemStack.prototype as any)._getId = procHacker.js("ItemStackBase::getId", RawTypeId.Int32, {this:ItemStack});
+(ItemStack.prototype as any)._getItem = procHacker.js("ItemStackBase::getItem", Item, {this:ItemStack});
 (ItemStack.prototype as any)._setCustomName = procHacker.js("ItemStackBase::setCustomName", RawTypeId.Void, {this:ItemStack}, CxxStringWrapper);
 ItemStack.prototype.getItem = procHacker.js("ItemStackBase::getItem", Item, {this:ItemStack});
 ItemStack.prototype.isBlock = procHacker.js("ItemStackBase::isBlock", RawTypeId.Boolean, {this:ItemStack});
