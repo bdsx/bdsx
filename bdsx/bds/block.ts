9a82bf466001e7b083871e6527f7cf0459640765
PneuJai <53138480+Rjlintkh@users.noreply.github.com>
2021-03-13 10:32:32 +0800

    add missing imports

--- bdsx/bds/block.ts
+++ bdsx/bds/block.ts
@@ -1,6 +1,7 @@
 import { NativeClass } from "bdsx/nativeclass";
 import { abstract } from "bdsx/common";
 import { HashedString } from "./hashedstring";
+import { CxxStringWrapper } from "bdsx/pointer";
 
 export class BlockLegacy extends NativeClass {
     protected _getCommandName(): CxxStringWrapper {
