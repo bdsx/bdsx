aec276f5a5f83178804c63bb7bec87ee324ba825
karikera <karikera3@hotmail.com>
2021-04-22 23:50:12 +0900

    checkmodules.ts - check complex ranges

--- bdsx/checkmodules.ts
+++ bdsx/checkmodules.ts
@@ -4,42 +4,86 @@ import fs = require('fs');
 import colors = require('colors');
 
 
-function checkVersion(pkgname:string, installed:string, required:string|undefined):boolean {
-    if (required === '*') return true;
-    if (required === undefined) return true;
+function checkVersion(installed:number[], required:string):boolean {
+    let ifGreater = false;
+    let ifEqual = false;
+    let ifLess = false;
+    let updateMinorVersion = false;
+    let updatePatchVersion = false;
 
-    switch (required.charAt(0)) {
-    case '~': case '^':
-        required = required.substr(1);
-        break;
+    const comparison = required.match(/^~|\^|>=?|<=?/);
+    if (comparison !== null) {
+        switch (comparison[0]) {
+        case '^':
+            updateMinorVersion = true;
+            ifEqual = true;
+            break;
+        case '~':
+            updatePatchVersion = true;
+            ifEqual = true;
+            break;
+        case '>=':
+            ifGreater = true;
+            ifEqual = true;
+            break;
+        case '>':
+            ifGreater = true;
+            break;
+        case '<=':
+            ifLess = true;
+            ifEqual = true;
+            break;
+        case '<':
+            ifLess = true;
+            break;
+        }
+        required = required.substr(comparison[0].length);
+    } else {
+        ifEqual = true;
     }
-    const matched = installed.match(/^([0-9]+)\.([0-9]+)\.([0-9]+)$/);
-    if (matched === null) {
-        throw Error(`${pkgname}: Invalid installed version string (${installed})`);
-    }
-    matched.shift();
 
-    const requiredArray = required.split('.');
-    let last = requiredArray[requiredArray.length-1];
-    if (requiredArray.length > 3) {
-        throw Error(`${pkgname}: Invalid version string (${required})`);
-    }
+    const requiredNums = required.split('.');
+    if (comparison === null && !/^[0-9]+$/.test(requiredNums[0])) return true;
 
+    let last = requiredNums[requiredNums.length-1];
     if (last === 'x') {
         for (;;) {
-            requiredArray.pop();
-            if (requiredArray.length === 0) return true;
-            last = requiredArray[requiredArray.length-1];
+            requiredNums.pop();
+            if (requiredNums.length === 0) return true;
+            last = requiredNums[requiredNums.length-1];
             if (last === 'x') continue;
             break;
         }
     }
-    for (let i=0;i<requiredArray.length;i++) {
-        const diff = +matched[i] - +requiredArray[i];
-        if (diff > 0) return true;
-        if (diff < 0) return false;
+    for (let i=0;i<requiredNums.length;i++) {
+        const diff = installed[i] - +requiredNums[i];
+        if (diff > 0) {
+            if (updateMinorVersion && i >= 1) return true;
+            if (updatePatchVersion && i >= 2) return true;
+            return ifGreater;
+        }
+        if (diff < 0) return ifLess;
     }
-    return true;
+    return ifEqual;
+}
+
+function checkVersionSyntax(pkgname:string, installed:string, requireds:string):boolean {
+    const installedSplited = installed.match(/^([0-9]+)\.([0-9]+)\.([0-9]+)$/);
+    if (installedSplited === null) {
+        throw Error(`${pkgname}: Invalid installed version string (${installed})`);
+    }
+    installedSplited.shift();
+    const installedNums = installedSplited.map(str=>+str);
+
+    for (const reqs of requireds.split(/ *\|\| */)) {
+        const [req1, req2] = reqs.split(/ *- */, 2);
+        if (req2 !== undefined) {
+            if (checkVersion(installedNums, req1) && checkVersion(installedNums, '<='+req2)) return true;
+        } else {
+            if (checkVersion(installedNums, req1)) return true;
+        }
+    }
+    return false;
 }
 
 const bdsxPath = path.join(__dirname, '..')+path.sep;
@@ -54,7 +98,7 @@ for (const name in requiredDeps) {
         const installed = require(`${name}/package.json`);
         const installedVersion = installed.version;
         const requiredVersion = requiredDeps[name];
-        if (!checkVersion(name, installedVersion, requiredVersion)) {
+        if (!checkVersionSyntax(name, installedVersion, requiredVersion)) {
             console.error(colors.red(`${name}: version does not match (installed=${installedVersion}, required=${requiredVersion})`));
             needUpdate = true;
         }
