b9ed46376579218e9008158701d2c8cfe20f4f24
Kuyakgaming <80567214+Kuyakgaming@users.noreply.github.com>
2021-11-05 09:12:03 +0800

    Create net-chat-to-console.js

---
+++ example_and_test/net-chat-to-console.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Chat Listening
+const packetids_1 = require("bdsx/bds/packetids");
+const common_1 = require("bdsx/common");
+const event_1 = require("bdsx/event");
+event_1.events.packetBefore(packetids_1.MinecraftPacketIds.Text).on(ev => {
+    if (ev.message === 'nochat') {
+        return common_1.CANCEL; // canceling
+    }
+    console.log(`[Chat] ${ev.message}`) // logging chat to console
+});
