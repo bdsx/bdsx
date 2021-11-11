4b64d19801e3fd0babe57dbebc1432c74b31f07b
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2021-11-11 12:52:51 +0700

    Update net-chat-to-console.ts
    Print name of author as well as message

--- example_and_test/net-chat-to-console.ts
+++ example_and_test/net-chat-to-console.ts
@@ -5,6 +5,6 @@ import { events } from "bdsx/event";
 import { system } from "./bedrockapi-system";
 
 events.packetBefore(MinecraftPacketIds.Text).on(ev => {
-    console.log(`[Chat] ${ev.message}`) //logging to console
+    console.log(`[Chat] <${ev.name}> ${ev.message}`) //logging to console
 	
 });
