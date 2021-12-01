91e72390c736055f932bb45c59d12937d4000a07
karikera <karikera3@hotmail.com>
2021-12-01 15:39:55 +0900

    re-enable examples

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 
 // Please start your own codes from here!
 
-// import './example_and_test'; // remove this if it's not necessary for you
+import './example_and_test'; // remove this if it's not necessary for you
