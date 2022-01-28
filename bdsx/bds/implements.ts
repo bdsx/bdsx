d5d43478479c7959eb962999f94df6aa97e26c0b
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-28 17:12:19 +0900

    `$` instaed of `_` as namespace resolution operator

--- bdsx/bds/implements.ts
+++ bdsx/bds/implements.ts
@@ -147,8 +147,8 @@ Spawner.prototype.spawnMob = function(region:BlockSource, id:ActorDefinitionIden
 
 // actor.ts
 const actorMaps = new Map<string, Actor>();
-const ServerPlayer_vftable = proc["ServerPlayer::`vftable'"];
-const ItemActor_vftable = proc["ItemActor::`vftable'"];
+const ServerPlayer$vftable = proc["ServerPlayer::`vftable'"];
+const ItemActor$vftable = proc["ItemActor::`vftable'"];
 Actor.abstract({
     vftable: VoidPointer,
     ctxbase: EntityContextBase,
@@ -167,9 +167,9 @@ Actor.setResolver(ptr=>{
     const binptr = ptr.getAddressBin();
     let actor = actorMaps.get(binptr);
     if (actor != null) return actor;
-    if (ptr.getPointer().equals(ServerPlayer_vftable)) {
+    if (ptr.getPointer().equals(ServerPlayer$vftable)) {
         actor = ptr.as(ServerPlayer);
-    } else if (ptr.getPointer().equals(ItemActor_vftable)) {
+    } else if (ptr.getPointer().equals(ItemActor$vftable)) {
         actor = ptr.as(ItemActor);
     } else {
         actor = ptr.as(Actor);
