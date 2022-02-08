0d444f751ffd16fec1e0c288cfa79fe9d1a1c15c
7dev7urandom <7dev7urandom@users.noreply.github.com>
2022-02-08 14:10:38 +0700

    Move vulnerability examples into separate file

--- example_and_test/vulnerabilities.ts
+++ example_and_test/vulnerabilities.ts
@@ -1,8 +1,21 @@
-import { NetworkIdentifier, ServerNetworkHandler } from "bdsx/bds/networkidentifier";
+import { ServerNetworkHandler, NetworkIdentifier } from "bdsx/bds/networkidentifier";
+import { MinecraftPacketIds } from "bdsx/bds/packetids";
+import { CANCEL } from "bdsx/common";
 import { pdb, VoidPointer } from "bdsx/core";
+import { events } from "bdsx/event";
 import { void_t } from "bdsx/nativetype";
 import { ProcHacker } from "bdsx/prochacker";
 
+// CVE-2021-45383
+events.packetRaw(MinecraftPacketIds.ClientCacheBlobStatus).on((ptr, size, netId) => {
+    if(ptr.readVarUint() > 0xff || ptr.readVarUint() > 0xff) {
+        console.log("DOS detected from " + netId);
+        netId.getActor()?.sendMessage("DOS (ClientCacheBlobStatus) detected");
+        return CANCEL;
+    }
+});
+
+// CVE-2021-45384
 const procHacker = ProcHacker.load("../pdbcache_by_example.ini", ["?handle@ServerNetworkHandler@@UEAAXAEBVNetworkIdentifier@@AEBVDisconnectPacket@@@Z"]);
 pdb.close();
 
