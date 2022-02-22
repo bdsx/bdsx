b3141c784a717f1b2bc67a1d5ba5fef94dab328d
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-02-22 10:19:21 +0900

    Revert "symbol `printf` is in `decoratedSymbols`"
    This reverts commit bb28ce59f707e8330ef85c4a79208819ee42aa8d.

--- bdsx/bds/symbollist.ts
+++ bdsx/bds/symbollist.ts
@@ -97,6 +97,7 @@ export const undecoratedSymbols = [
     'Json::Value::size',
     'Json::Value::resolveReference',
     'MinecraftServerScriptEngine::onServerUpdateEnd',
+    'printf',
     "ServerCommandOrigin::`vftable'",
     "ScriptCommandOrigin::`vftable'",
     'Minecraft::getLevel',
@@ -450,7 +451,6 @@ export const undecoratedSymbols = [
 export const decoratedSymbols = [
     'sprintf',
     'vsnprintf',
-    'printf',
     '??$_Allocate@$0BA@U_Default_allocate_traits@std@@$0A@@std@@YAPEAX_K@Z',
     '?ToString@SystemAddress@RakNet@@QEBAX_NPEADD@Z',
     '??0?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QEAA@XZ',
