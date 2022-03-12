ae8579c0ee99e8ddf329db25b54c6e8d5699f563
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-03-13 00:23:47 +0900

    Revert "use `Actor::startRiding`"
    This reverts commit 0e08699156696560d37ae9b4f46d4cf2c36e4dc3.

--- bdsx/bds/implements.ts
+++ bdsx/bds/implements.ts
@@ -263,7 +263,7 @@ Actor.prototype.isMoving = procHacker.js("Actor::isMoving", bool_t, {this:Actor}
 Actor.prototype.setSneaking = procHacker.js("Actor::setSneaking", void_t, {this:Actor}, bool_t);
 Actor.prototype.getHealth = procHacker.js("Actor::getHealth", int32_t, {this:Actor});
 Actor.prototype.getMaxHealth = procHacker.js("Actor::getMaxHealth", int32_t, {this:Actor});
-Actor.prototype.startRiding = procHacker.js("Actor::startRiding", bool_t, {this:Actor}, Actor);
+Actor.prototype.startRiding = makefunc.js([0x1a8], bool_t, {this:Actor}, Actor);
 
 const Actor$save = procHacker.js("Actor::save", bool_t, {this:Actor}, CompoundTag);
 Actor.prototype.save = function(tag?:CompoundTag):any {
