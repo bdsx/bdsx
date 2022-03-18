7e7ee21347116404acfb9004bbf913ade8b2cdc3
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-03-18 20:04:03 +0900

    update example, net-disconnect

--- example_and_test/net-disconnect.ts
+++ example_and_test/net-disconnect.ts
@@ -1,5 +1,9 @@
 
 // Network Hooking: disconnected
+import { PlayerCommandSelector } from "bdsx/bds/command";
+import { ServerPlayer } from "bdsx/bds/player";
+import { serverInstance } from "bdsx/bds/server";
+import { command } from "bdsx/command";
 import { events } from "bdsx/event";
 import { connectionList } from "./net-login";
 
@@ -7,4 +11,13 @@ events.networkDisconnected.on(networkIdentifier => {
     const id = connectionList.get(networkIdentifier);
     connectionList.delete(networkIdentifier);
     console.log(`${id}> disconnected`);
+});
+
+command.register("disconnect", "disconnect player").overload((p, o, op) => {
+    for (const player of p.target.newResults(o, ServerPlayer)) {
+        // disconnect player from server
+        serverInstance.disconnectClient(player.getNetworkIdentifier());
+    }
+}, {
+    target:PlayerCommandSelector
 });
