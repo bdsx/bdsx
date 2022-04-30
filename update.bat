697f99e92bf251a330629417502310e2b3eda584
karikera <karikera3@hotmail.com>
2022-04-30 20:34:58 +0900

    update.bat - errorlevel=1 on failed

--- update.bat
+++ update.bat
@@ -5,7 +5,7 @@ cd /D "%~dp0"
 where npm >nul 2>&1
 if %errorlevel% neq 0 (
     echo Error: bdsx requires npm. Please install node.js first 1>&2
-    exit /b 0
+    exit /b 1
 )
 echo ^> npm i
 call npm i
