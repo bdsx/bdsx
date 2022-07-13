d3290c417a99d3a2193961a9f146a1c0f6b597cd
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-07-14 02:03:24 +0900

    default value getRemoved - getRuntimeEntity

--- bdsx/bds/implements.ts
+++ bdsx/bds/implements.ts
@@ -140,7 +140,10 @@ Level.prototype.getEntities = function() {
     return out;
 };
 
-Level.prototype.getRuntimeEntity = procHacker.js("?getRuntimeEntity@Level@@UEBAPEAVActor@@VActorRuntimeID@@_N@Z", Actor, {this:Level}, ActorRuntimeID, bool_t);
+const Level$getRuntimeEntity = procHacker.js("?getRuntimeEntity@Level@@UEBAPEAVActor@@VActorRuntimeID@@_N@Z", Actor, null, Level, ActorRuntimeID, bool_t);
+Level.prototype.getRuntimeEntity = function (id, getRemoved = false) {
+    return Level$getRuntimeEntity(this, id, getRemoved);
+};
 Level.prototype.getRuntimePlayer = procHacker.js("?getRuntimePlayer@Level@@UEBAPEAVPlayer@@VActorRuntimeID@@@Z", Player, {this:Level}, ActorRuntimeID);
 Level.prototype.getTime = procHacker.js("?getTime@Level@@UEBAHXZ", int64_as_float_t, {this:Level});
 Level.prototype.getCurrentTick = procHacker.js("?getCurrentTick@Level@@UEBAAEBUTick@@XZ", int64_as_float_t.ref(), {this:Level});// You can run the server for 1.4202551784875594e+22 years till it exceeds the max safe integer
