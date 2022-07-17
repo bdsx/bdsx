68098d00fb5aba189c302893ae273424682a3778
PTKDrake <kien192837@gmail.com>
2022-07-17 16:53:55 +0700

    Fix

--- bdsx/bds/blockpos.ts
+++ bdsx/bds/blockpos.ts
@@ -22,33 +22,33 @@ export namespace Facing {
 @nativeClass()
 export class BlockPos extends NativeStruct {
     @nativeField(int32_t)
-    x: int32_t;
+    x:int32_t;
     @nativeField(int32_t)
-    y: int32_t;
+    y:int32_t;
     @nativeField(int32_t)
-    z: int32_t;
+    z:int32_t;
 
-    set(pos: { x: number, y: number, z: number }): void {
+    set(pos:{x:number, y:number, z:number}):void {
         this.x = pos.x;
         this.y = pos.y;
         this.z = pos.z;
     }
 
-    equal(pos: BlockPos | Vec3): boolean {
+    equal(pos: BlockPos|Vec3): boolean{
         return this.x === pos.x && this.y === pos.y && this.z === pos.z;
     }
 
-    abs(): BlockPos {
+    abs(): BlockPos{
         return BlockPos.create(Math.abs(this.x), Math.abs(this.y), Math.abs(this.z));
     }
 
     inc(pos: BlockPos): BlockPos;
     inc(pos: Vec3): BlockPos;
     inc(x: number, y: number, z: number): BlockPos;
-    inc(a: number | Vec3 | BlockPos, b: number = 0, c: number = 0): BlockPos {
-        if (typeof a === 'number') {
-            return BlockPos.create(this.x + Math.round(a), this.y + Math.round(b), this.z + Math.round(c));
-        } else {
+    inc(a: number|Vec3|BlockPos, b: number = 0, c: number = 0): BlockPos{
+        if(typeof a === 'number'){
+            return BlockPos.create(this.x + (a | 0), this.y + (b | 0), this.z + (c | 0));
+        }else{
             return this.inc(a.x, a.y, a.z);
         }
     }
@@ -56,35 +56,35 @@ export class BlockPos extends NativeStruct {
     dec(pos: BlockPos): BlockPos;
     dec(pos: Vec3): BlockPos;
     dec(x: number, y: number, z: number): BlockPos;
-    dec(a: number | Vec3 | BlockPos, b: number = 0, c: number = 0): BlockPos {
-        if (typeof a === 'number') {
+    dec(a: number|Vec3|BlockPos, b: number = 0, c: number = 0): BlockPos{
+        if(typeof a === 'number'){
             return this.inc(-a, -b, -c);
-        } else {
+        }else{
             return this.inc(-a.x, -a.y, -a.z);
         }
     }
 
-    multiply(times: number): BlockPos {
+    multiply(times: number): BlockPos{
         return BlockPos.create((this.x * times) | 0, this.y * times, (this.z * times) | 0);
     }
 
-    divide(times: number): BlockPos {
+    divide(times: number): BlockPos{
         return BlockPos.create((this.x / times) | 0, (this.y / times) | 0, (this.z / times) | 0);
     }
 
-    lengthSquared(): number {
+    lengthSquared(): number{
         return this.x * this.x + this.y * this.y + this.z * this.z;
     }
 
-    normalize(): BlockPos {
+    normalize(): BlockPos{
         const len = this.lengthSquared();
-        if (len > 0) {
+        if(len > 0){
             return this.divide(Math.sqrt(len));
         }
         return BlockPos.create(0, 0, 0);
     }
 
-    getSide(side: Facing, step: number = 1): BlockPos {
+    getSide(side: Facing, step: number = 1): BlockPos{
         switch (side) {
         case Facing.Down:
             return this.dec(0, step, 0);
@@ -103,16 +103,16 @@ export class BlockPos extends NativeStruct {
         }
     }
 
-    relative(facing: Facing, steps: number): BlockPos {
+    relative(facing:Facing, steps:number):BlockPos {
         abstract();
     }
 
     static create(pos: Vec3): BlockPos;
     static create(pos: BlockPos): BlockPos;
-    static create(pos: { x: number, y: number, z: number }): BlockPos;
-    static create(x: number, y: number, z: number): BlockPos;
-    static create(a: number | { x: number, y: number, z: number }, b: number = 0, c: number = 0): BlockPos {
-        if (typeof a === "number") {
+    static create(pos: {x:number, y:number, z:number}): BlockPos;
+    static create(x:number, y:number, z:number):BlockPos;
+    static create(a:number|{x:number, y:number, z:number}, b:number=0, c:number=0):BlockPos {
+        if(typeof a === "number") {
             const v = new BlockPos(true);
             v.x = Math.floor(a);
             v.y = Math.floor(b);
@@ -123,32 +123,32 @@ export class BlockPos extends NativeStruct {
         }
     }
 
-    toJSON(): VectorXYZ {
-        return { x: this.x, y: this.y, z: this.z };
+    toJSON():VectorXYZ {
+        return {x:this.x, y:this.y, z:this.z};
     }
 }
 
-BlockPos.prototype.relative = procHacker.js("?relative@BlockPos@@QEBA?AV1@EH@Z", BlockPos, { this: BlockPos, structureReturn: true }, uint8_t, int32_t);
+BlockPos.prototype.relative = procHacker.js("?relative@BlockPos@@QEBA?AV1@EH@Z", BlockPos, {this:BlockPos, structureReturn:true}, uint8_t, int32_t);
 
 @nativeClass()
 export class ChunkPos extends NativeStruct {
     @nativeField(int32_t)
-    x: int32_t;
+    x:int32_t;
     @nativeField(int32_t)
-    z: int32_t;
+    z:int32_t;
 
-    set(pos: ChunkPos | { x: number, z: number }): void {
+    set(pos:ChunkPos|{x:number, z:number}):void {
         this.x = pos.x;
         this.z = pos.z;
     }
 
-    static create(x: number, z: number): ChunkPos;
-    static create(pos: BlockPos): ChunkPos;
-    static create(a: number | BlockPos, b: number = 0): ChunkPos {
+    static create(x:number, z:number):ChunkPos;
+    static create(pos:BlockPos):ChunkPos;
+    static create(a:number|BlockPos, b?:number):ChunkPos {
         const v = new ChunkPos(true);
         if (typeof a === "number") {
             v.x = a;
-            v.z = b;
+            v.z = b!;
         } else {
             v.x = a.x >> 4;
             v.z = a.z >> 4;
@@ -156,34 +156,34 @@ export class ChunkPos extends NativeStruct {
         return v;
     }
 
-    toJSON(): { x: number, z: number } {
-        return { x: this.x, z: this.z };
+    toJSON():{x: number, z: number} {
+        return {x:this.x, z:this.z};
     }
 }
 
 @nativeClass()
 export class ChunkBlockPos extends NativeStruct {
     @nativeField(uint8_t)
-    x: uint8_t;
+    x:uint8_t;
     @nativeField(uint16_t)
-    y: uint16_t;
+    y:uint16_t;
     @nativeField(uint8_t)
-    z: uint8_t;
+    z:uint8_t;
 
-    set(pos: ChunkBlockPos | { x: number, y: number, z: number }): void {
+    set(pos:ChunkBlockPos|{x:number, y:number, z:number}):void {
         this.x = pos.x;
         this.y = pos.y;
         this.z = pos.z;
     }
 
-    static create(x: number, y: number, z: number): ChunkBlockPos;
-    static create(pos: BlockPos): ChunkBlockPos;
-    static create(a: number | BlockPos, b: number = 0, c: number = 0): ChunkBlockPos {
+    static create(x:number, y:number, z:number):ChunkBlockPos;
+    static create(pos:BlockPos):ChunkBlockPos;
+    static create(a:number|BlockPos, b?:number, c?:number):ChunkBlockPos {
         const v = new ChunkBlockPos(true);
         if (typeof a === "number") {
             v.x = a;
-            v.y = b;
-            v.z = c;
+            v.y = b!;
+            v.z = c!;
         } else {
             v.x = a.x & 0xF;
             v.y = a.y;
@@ -192,124 +192,124 @@ export class ChunkBlockPos extends NativeStruct {
         return v;
     }
 
-    toJSON(): VectorXYZ {
-        return { x: this.x, y: this.y, z: this.z };
+    toJSON():VectorXYZ {
+        return {x:this.x, y:this.y, z:this.z};
     }
 }
 
 @nativeClass()
 export class Vec2 extends NativeStruct {
     @nativeField(float32_t)
-    x: float32_t;
+    x:float32_t;
     @nativeField(float32_t)
-    y: float32_t;
+    y:float32_t;
 
-    set(pos: Vec2 | { x: number, y: number }): void {
+    set(pos:Vec2|{x:number, y:number}):void {
         this.x = pos.x;
         this.y = pos.y;
     }
 
-    static create(x: number, y: number): Vec2 {
+    static create(x:number, y:number):Vec2 {
         const v = new Vec2(true);
         v.x = x;
         v.y = y;
         return v;
     }
 
-    toJSON(): { x: number, y: number } {
-        return { x: this.x, y: this.y };
+    toJSON():{x: number, y: number} {
+        return {x:this.x, y:this.y};
     }
 }
 
 @nativeClass()
 export class Vec3 extends NativeStruct {
     @nativeField(float32_t)
-    x: float32_t;
+    x:float32_t;
     @nativeField(float32_t)
-    y: float32_t;
+    y:float32_t;
     @nativeField(float32_t)
-    z: float32_t;
+    z:float32_t;
 
-    set(pos: Vec3 | BlockPos | { x: number, y: number, z: number }): void {
+    set(pos:Vec3|BlockPos|{x:number, y:number, z:number}):void {
         this.x = pos.x;
         this.y = pos.y;
         this.z = pos.z;
     }
 
-    distance(pos: Vec3 | BlockPos | { x: number, y: number, z: number }): number {
+    distance(pos:Vec3|BlockPos|{x:number, y:number, z:number}):number {
         return Math.sqrt(this.distanceSq(pos));
     }
 
-    distanceSq(pos: Vec3 | BlockPos | { x: number, y: number, z: number }): number {
+    distanceSq(pos:Vec3|BlockPos|{x:number, y:number, z:number}):number {
         const xdist = (this.x - pos.x);
         const ydist = (this.y - pos.y);
         const zdist = (this.z - pos.z);
-        return xdist * xdist + ydist * ydist + zdist * zdist;
+        return xdist*xdist + ydist*ydist + zdist*zdist;
     }
 
-    equal(pos: Vec3 | BlockPos): boolean {
+    equal(pos: Vec3|BlockPos): boolean{
         return this.x === pos.x && this.y === pos.y && this.z === pos.z;
     }
 
-    floor(): Vec3 {
+    floor(): Vec3{
         return Vec3.create(Math.floor(this.x), Math.floor(this.y), Math.floor(this.z));
     }
 
-    ceil(): Vec3 {
+    ceil(): Vec3{
         return Vec3.create(Math.ceil(this.x), Math.ceil(this.y), Math.ceil(this.z));
     }
 
-    round(): Vec3 {
+    round(): Vec3{
         return Vec3.create(Math.round(this.x), Math.round(this.y), Math.round(this.z));
     }
 
-    abs(): Vec3 {
+    abs(): Vec3{
         return Vec3.create(Math.abs(this.x), Math.abs(this.y), Math.abs(this.z));
     }
 
     inc(pos: BlockPos): Vec3;
     inc(pos: Vec3): Vec3;
     inc(x: number, y: number, z: number): Vec3;
-    inc(a: number | Vec3 | BlockPos, b: number = 0, c: number = 0): Vec3 {
-        if (typeof a === 'number') {
+    inc(a: number|Vec3|BlockPos, b: number = 0, c: number = 0): Vec3{
+        if(typeof a === 'number'){
             return Vec3.create(this.x + a, this.y + b, this.z + c);
-        } else {
-            return Vec3.create(this.x + a.x, this.y + a.y, this.z + a.z);
+        }else{
+            return this.inc(a.x, a.y, a.z);
         }
     }
 
     dec(pos: BlockPos): Vec3;
     dec(pos: Vec3): Vec3;
     dec(x: number, y: number, z: number): Vec3;
-    dec(a: number | Vec3 | BlockPos, b: number = 0, c: number = 0): Vec3 {
-        if (typeof a === 'number') {
+    dec(a: number|Vec3|BlockPos, b: number = 0, c: number = 0): Vec3{
+        if(typeof a === 'number'){
             return this.inc(-a, -b, -c);
-        } else {
+        }else{
             return this.inc(-a.x, -a.y, -a.z);
         }
     }
 
-    multiply(times: number): Vec3 {
+    multiply(times: number): Vec3{
         return Vec3.create(this.x * times, this.y * times, this.z * times);
     }
 
-    divide(times: number): Vec3 {
+    divide(times: number): Vec3{
         return Vec3.create(this.x / times, this.y / times, this.z / times);
     }
 
-    lengthSquared(): number {
+    lengthSquared(): number{
         return this.x * this.x + this.y * this.y + this.z * this.z;
     }
 
-    normalize(): Vec3 {
+    normalize(): Vec3{
         const len = this.lengthSquared();
-        if (len > 0) {
+        if(len > 0){
             return this.divide(Math.sqrt(len));
         }
         return Vec3.create(0, 0, 0);
     }
 
-    getSide(side: Facing, step: number = 1): Vec3 {
+    getSide(side: Facing, step: number = 1): Vec3{
         switch (side) {
         case Facing.Down:
             return this.dec(0, step, 0);
@@ -330,14 +330,14 @@ export class Vec3 extends NativeStruct {
 
     static create(pos: Vec3): Vec3;
     static create(pos: BlockPos): Vec3;
-    static create(x: number, y: number, z: number): Vec3;
-    static create(a: number | BlockPos | Vec3, b: number = 0, c: number = 0): Vec3 {
+    static create(x:number, y:number, z:number):Vec3;
+    static create(a:number|BlockPos|Vec3, b?:number, c?:number):Vec3 {
         const v = new Vec3(true);
-        if (typeof a === 'number') {
+        if(typeof a === 'number'){
             v.x = a;
-            v.y = b;
-            v.z = c;
-        } else {
+            v.y = b!;
+            v.z = c!;
+        }else{
             v.x = a.x;
             v.y = a.y;
             v.z = a.z;
@@ -345,29 +345,29 @@ export class Vec3 extends NativeStruct {
         return v;
     }
 
-    toJSON(): VectorXYZ {
-        return { x: this.x, y: this.y, z: this.z };
+    toJSON():VectorXYZ {
+        return {x:this.x, y:this.y, z:this.z};
     }
 }
 
 @nativeClass()
 export class RelativeFloat extends NativeStruct {
-    static readonly [CommandParameterType.symbol]: true;
+    static readonly [CommandParameterType.symbol]:true;
     static readonly [NativeType.registerDirect] = true;
     @nativeField(float32_t)
-    value: float32_t;
+    value:float32_t;
     @nativeField(bool_t)
-    is_relative: bool_t;
+    is_relative:bool_t;
 
     @nativeField(bin64_t, 0)
-    bin_value: bin64_t;
+    bin_value:bin64_t;
 
-    set(pos: RelativeFloat | { value: number, is_relative: boolean }): void {
+    set(pos:RelativeFloat|{value:number, is_relative:boolean}):void {
         this.value = pos.value;
         this.is_relative = pos.is_relative;
     }
 
-    static create(value: number, is_relative: boolean): RelativeFloat {
+    static create(value:number, is_relative:boolean):RelativeFloat {
         const v = new RelativeFloat(true);
         v.value = value;
         v.is_relative = is_relative;
