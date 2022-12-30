7fe76b2f6f979fc3ef7986b3f292fe6d1bfa3193
Grape <sacrigrape@gmail.com>
2022-12-30 00:45:41 -0600

    Fixed index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,4 @@
 
 // Please start your own codes from here!
 
-//import './example_and_test';
-import {events} from 'bdsx/event';
-import {PistonAction} from 'bdsx/event_impl/blockevent';
-
-events.pistonMove.on((ev) => {
-    console.log(ev.blockPos);
-    console.log(ev.affectedBlocks);
-    console.log(PistonAction[ev.action]);
-})
+import './example_and_test';
