7f126b6ef31927975bf03783fe63010f44af1749
karikera <karikera3@hotmail.com>
2023-01-18 02:48:11 +0900

    update.bat/sh - return error if fail

--- update.sh
+++ update.sh
@@ -6,9 +6,12 @@ cd $(dirname "$SCRIPT")
 
 if ! [ -x "$(command -v npm)" ]; then
     echo 'Error: bdsx requires npm. Please install node.js first' >&2
-    exit 0
+    cd $cwd
+    exit 1
 fi
 echo "> npm i --unsafe-perm"
 npm i --unsafe-perm
 
+res=$?
 cd $cwd
+exit $res
